05a8ff97dea9b62e3bfe790c479769d8e63679a1
Joe Conwell <jmc529@vt.edu>
2022-12-29 02:25:44 -0500

    fixed position issues

--- betterTurtleAPI.lua
+++ betterTurtleAPI.lua
@@ -4,9 +4,9 @@ Pastebin: MdD051uF
     Functions:
         calibrate() to the cardinal directions
         boxTo(destinationVector[, fn]) moves to every location between two coordinates
-        getPostion() returns the turtle's position as a vector
+        getPosition() returns the turtle's position as a vector
         goTo(vector[, fn]) moves the turtle to a specific vector
-        move(direction[, fn][, count]) multiple times, foward-back-up-down
+        move(direction[, fn][, count]) multiple times, forward-back-up-down
         refuel() checks inventory for fuel
         xMovement(distance[, fn]) moves the turtle along the x-axis
         yMovement(distance[, fn]) moves the turtle along the y-axis
@@ -23,8 +23,8 @@ done when the API is loaded and shouldn't be edited out.
 This API should be used with lua's require method
 
 @author informer
-@date February 14th 2019
-@version 0.1.2
+@date November 9th 2022
+@version 0.1.3
 ]]--
 local BTA = {}
 ---Local Variables---
@@ -40,18 +40,18 @@ function BTA.calibrate()
     if gps.locate(5) == nil then
         gpsMode = false
         print("Please enter in the exact coordinates of the turtle\nX:")
-        x = tonumber(read())
+        local x = tonumber(read())
         print("\nY:")
-        y = tonumber(read())
+        local y = tonumber(read())
         print("\nZ:")
-        z = tonumber(read())
+        local z = tonumber(read())
         position = vector.new(x, y, z)
         print("Please enter the direction the turtle is facing <N,E,W,S>:")
-        direction = read()
+        local direction = read()
         if not direction:find("N") and not direction:find("E") and not direction:find("W") and not direction:find("S") then
             BTA.calibrate()
         end
-        dirDic = {["N"] = 0, ["E"] = 1, ["S"] = 2, ["W"] = 3}
+        local dirDic = {["N"] = 0, ["E"] = 1, ["S"] = 2, ["W"] = 3}
         currentDirection = dirDic[direction]
 
     else
@@ -78,7 +78,7 @@ function BTA.calibrate()
 end
 
 --[[
-Will move the turtle to the specifed vector, hitting every cooridnate in between the two points.
+Will move the turtle to the specified vector, hitting every coordinate in between the two points.
 @parameter destinationVector the coordinates to travel to
 @optionalParameter fn a function to apply at each step
 @return true if the turtle is able to travel to the vector
@@ -96,13 +96,13 @@ function BTA.boxTo(destinationVector, fn)
     --[[
     Helper function to move along a layer. Width and length should be found by subtracting the turtle's
     current location from it's next destination location.
-    @oarameter y the y value to add
+    @parameter y the y value to add
     @parameter to the vector we are heading to
     @parameter from the vector we are coming from
     ]]--
     local function addLayer(y, to, from)
         local length = to - from
-        local switch, x, z = 0
+        local switch, x, z = 0, 0, 0
         for i=0, math.abs(length.x) do
             x = (length.x > 0) and (from.x + i) or (from.x - i)
             z = (switch > 1) and from.z or to.z
@@ -116,7 +116,7 @@ function BTA.boxTo(destinationVector, fn)
         addLayer(location.y, destinationVector, location)
     else
         local ySpaces = (distance.y > 0) and 1 or -1
-        local locY, to, from, y, temp = location.y, destinationVector, location
+        local locY, to, from, y, temp = location.y, destinationVector, location, 0, 0
         for i=0, math.abs(distance.y) do
             y = (distance.y > 0) and (locY + i) or (locY - i)
             addLayer(y, to, from)
@@ -131,16 +131,16 @@ function BTA.boxTo(destinationVector, fn)
         BTA.goTo(coordinates[i], fn)
     end
 
-    --odd fix (ends up on oppisite side)
-    if location:dot(destinationVector)%2 == 1 then
+    --odd fix (ends up on opposite side)
+    if location:dot(destinationVector) % 2 == 1 then
         return BTA.goTo(destinationVector)
     end
 
     --check for truth
     local location
-    if gpsMode then 
+    if gpsMode then
         location = vector.new(gps.locate(5))
-    else 
+    else
         location = position
     end
 
@@ -153,16 +153,16 @@ function BTA.boxTo(destinationVector, fn)
 end
 
 --[[
-Returns the turtles postion, the relevance of this is mainly for when you 
-are working in a nonegps enviorment.
-@return postion of the turtle as a vector
+Returns the turtles position, the relevance of this is mainly for when you 
+are working in a non-gps environment.
+@return position of the turtle as a vector
 ]]--
-function BTA.getPostion()
-    return gpsMode and vector.new(gps.locate(5)) or postion
+function BTA.getPosition()
+    return gpsMode and vector.new(gps.locate(5)) or position
 end
 
 --[[
-Will move the turtle to the specifed vector.
+Will move the turtle to the specified vector.
 @parameter destinationVector the coordinates to travel to
 @optionalParameter fn a function to apply at each step
 @return true if the turtle is able to travel to the vector
@@ -175,12 +175,12 @@ function BTA.goTo(destinationVector, fn)
         location = position
     end
     local distance = destinationVector - location
-       
+
     BTA.yMovement(distance.y, fn)
     BTA.xMovement(distance.x, fn)
     BTA.zMovement(distance.z, fn)
 
-    local location
+    --validate location is correct
     if gpsMode then
         location = vector.new(gps.locate(5))
     else
@@ -204,7 +204,7 @@ during each movement.
 @return true if unimpeded; if false returns the remaining count
 ]]--
 function BTA.move(direction, fn, count)
-    local count = (count ~= nil) and count or 1
+    count = (count ~= nil) and count or 1
     if fn ~= nil then
         fn()
     end
@@ -255,22 +255,21 @@ Gets the direction the turtle is currently facing.
 @return number representing the direction the turtle is facing
 ]]--
 function turn.getDirection()
-    local direction = nil
-    local currDir = currentDirection % 4
-    if currDir == 0 then
+    local direction = currentDirection % 4
+    if direction == 0 then
         return "north"
-    elseif currDir == 1 then
+    elseif direction == 1 then
         return "east"
-    elseif currDir == 2 then
+    elseif direction == 2 then
         return "south"
-    elseif currDir == 3 then
+    elseif direction == 3 then
         return "west"
     end
 end
 
 --[[
 Turns the turtle to face a cardinal direction. 
-@parameter str a string representing the cardinal direciton to turn the turtle towards
+@parameter str a string representing the cardinal direction to turn the turtle towards
 ]]--
 function turn.toCardinal(str)
     if string.lower(str) == "north" then
@@ -290,8 +289,8 @@ Turns the turtle in a set direction n times
 @optionalParameter count the amount of times to turn the turtle
 ]]--
 function turn.spin(direction, count)
-    local count = (count ~= nil) and count or 1
-    
+    count = (count ~= nil) and count or 1
+
     if direction == -1 or string.lower(direction) == "left" then
         while (count > 0) do
             turtle.turnLeft()
@@ -327,7 +326,7 @@ end
 
 function turn.west()
     local n = currentDirection % 4
-    if (n-1) ~= -1 then 
+    if (n-1) ~= -1 then
         turn.spin(1, math.abs(n-3))
     else
         turn.spin(-1)
@@ -362,10 +361,10 @@ function BTA.refuel()
     for i=1,16 do
         turtle.select(i)
         if turtle.refuel(0) then
-            while turtle.getFuelLevel() < turtle.getFuelLimit() and turtle.getItemCount(i) > 0 do
+            while turtle.getFuelLevel() < limit and turtle.getItemCount(i) > 0 do
                 turtle.refuel(1)
             end
-            return true, turtle.getFuelLevel()            
+            return true, turtle.getFuelLevel()
         end
     end
 
@@ -373,82 +372,67 @@ function BTA.refuel()
 end
 
 --[[
-oves the turtle along the X axis.
-    *distance can be found by subtracting your destiation vector from your
+Moves the turtle along the X axis.
+    *distance can be found by subtracting your destination vector from your
     location vector
 @parameter distance the amount of blocks to move the turtle
 @optionalParameter fn a function to apply at each step
 @return true if the turtle is unimpeded
 ]]--
 function BTA.xMovement(distance, fn)
+    local completed, count
     if distance < 0 then
         BTA.turn.west()
-        local truth, count = BTA.move("forward", fn,  math.abs(distance))
+        completed, count = BTA.move("forward", fn,  math.abs(distance))
         position.x = position.x - math.abs(distance)
-        if not truth then 
-            return false
-        end
     elseif distance > 0 then
         BTA.turn.east()
-        local truth, count = BTA.move("forward", fn,  math.abs(distance))
+        completed, count = BTA.move("forward", fn,  math.abs(distance))
         position.x = position.x + math.abs(distance)
-        if not truth then 
-            return false
-        end
     end
-    return true
+    return completed, count
 end
 
 --[[
 Moves the turtle along the Y axis.
-    *distance can be found by subtracting your destiation vector from your
+    *distance can be found by subtracting your destination vector from your
     location vector
 @parameter distance the amount of blocks to move the turtle
 @optionalParameter fn a function to apply at each step
 @return true if the turtle is unimpeded
 ]]--
 function BTA.yMovement(distance, fn)
+    local completed, count
     if distance < 0 then
-        local truth, count = BTA.move("down", fn,  math.abs(distance))
+        completed, count = BTA.move("down", fn,  math.abs(distance))
         position.y = position.y - math.abs(distance)
-        if not truth then 
-            return false
-        end
     elseif distance > 0 then
-        local truth, count = BTA.move("up", fn,  math.abs(distance))
+        completed, count = BTA.move("up", fn,  math.abs(distance))
         position.y = position.y + math.abs(distance)
-        if not truth then
-            return false
-        end
     end
-    return true
+    return completed, count
 end
 
 --[[
 Moves the turtle along the Z axis.
-    *distance can be found by subtracting your destiation vector from your
+    *distance can be found by subtracting your destination vector from your
     location vector
 @parameter distance the amount of blocks to move the turtle
 @optionalParameter fn a function to apply at each step
 @return true if the turtle is unimpeded
 ]]--
 function BTA.zMovement(distance, fn)
+    local completed, count
     if distance < 0 then
         BTA.turn.north()
-        local truth, count = BTA.move("forward", fn,  math.abs(distance))
+        completed, count = BTA.move("forward", fn,  math.abs(distance))
         position.z = position.z - math.abs(distance)
-        if not truth then
-            return false
-        end
     elseif distance > 0 then
         BTA.turn.south()
-        local truth, count = BTA.move("forward", fn,  math.abs(distance))
+        completed, count = BTA.move("forward", fn,  math.abs(distance))
         position.z = position.z + math.abs(distance)
-        if not truth then
-            return false
-        end
     end
-    return true
+    return completed, count
 end
 
 BTA.calibrate()
