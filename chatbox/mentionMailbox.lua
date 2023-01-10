47a99fa95278419527bb305ca28d6374d518ba39
Joe Conwell <jmc529@vt.edu>
2023-01-09 21:16:15 -0500

    edited chatbox programs

--- chatbox/mentionMailbox.lua
+++ chatbox/mentionMailbox.lua
@@ -16,12 +16,15 @@ end
 parallel.waitForAny(
     function ()
         while true do
-            local event, user, message, data, time = os.pullEvent("chat")
+            local event, user, message, data = os.pullEvent("chat")
             if message:find("informer") then
                 if not excluded[user:lower()] then
                     chatbox.tell(user, "Informer is AFK right now. This mention has been recorded and they will see it when they return. Contact @Josee if you need faster response time.", BOT_NAME)
                     local m = fs.open(MESSAGES_PATH, "a")
-                    m.write(time.."|"..message)
+                    local info = user.."|"..data.time.."|"..message
+                    m.write(info)
+                    m.write("\n")
+                    print(info)
                     m.close()
                 end
             end
@@ -29,14 +32,15 @@ parallel.waitForAny(
     end,
     function ()
         while true do
-            
             local event, user, command, args = os.pullEvent("command")
-            if command and #args > 1 then
-                local opt = fs.open(OPT_OUT_PATH, "a")
-                opt.write(user:lower())
-                opt.close()
-            else
-                chatbox.tell(user, "Informer's AFK chatbot. Mentions shown to them upon return. Opt out with '\\i-afk opt-out'", BOT_NAME)
+            if command == "i-afk" then
+                if args[1] "opt-out" then
+                    local opt = fs.open(OPT_OUT_PATH, "a")
+                    opt.write(user:lower())
+                    opt.close()
+                else
+                    chatbox.tell(user, "Informer's AFK chatbot. Mentions shown to them upon return. Opt out with '\\i-afk opt-out'", BOT_NAME)
+                end
             end
         end
     end
