51f4f1b95eee59b4c0a17e7b94980ac5babe3afc
Joe Conwell <jmc529@vt.edu>
2023-01-28 06:57:22 -0500

    renamed farmer

--- turtles/microFarmer.lua
+++ turtles/microFarmer.lua
@@ -1,14 +1,8 @@
-local bta = require("bta")
-
-if (bta == nil) then
-	shell.run("pastebin get MdD051uF bta")
-	bta = require("bta")
-end
-
 local seeds = { "minecraft:seeds", "minecraft:carrots", "minecraft:potato", "minecraft:beetroot_seed"}
 local currentSeed = 1
 
 local function buildFarm()
+	
 end
 
 local function plantSeeds()
