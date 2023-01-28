f122789578f7fed55ce44773f998b8bbd0ff94d3
Joe Conwell <jmc529@vt.edu>
2023-01-28 06:56:31 -0500

    fixed logging and added failsafe for restarts

--- Plethora/autoFish.lua
+++ Plethora/autoFish.lua
@@ -8,42 +8,46 @@ local swap = -1
 
 -- Hard coded thresholds that effect catch likelihood
 local dataCount = 3 -- how many time sea level is checked, 3 to 5 seems good
-local dipThreshhold = 0.08 -- how low bobber has to get before reeling in, varies greatly
+local dipThreshold = 0.08 -- how low bobber has to get before reeling in, varies greatly
 local castTime = 2 -- how long to wait after casting to begin the while loop, increase if you have a large cast time
 local turnRadius = 10 -- turn to avoid getting bobber caught on fish entity on the way out
 
 -- Data logging, for debugging
-local c = ni.canvas()
 local y = 5
 local x = 25
-local cWidth, cHeight = c.getSize()
+local c = ni.canvas()
+local cWidth, _ = c.getSize()
 local logging = false
 local fontSize = 0.5
-local lineHeight = 4
+
+local function log(str)
+    c.clear()
+    c.addText({ cWidth - string.len(str) - x, y}, str, 0xFFFFFFFF, fontSize)
+end
 
 local function findSeaLevel()
     local seaLevels = {}
     for _, entity in pairs(ni.sense()) do
-        if entity.key ==  "minecraft:fishing_bobber" then
+        if entity.key == "minecraft:fishing_bobber" then
             for i = dataCount, 1, -1 do
-                for _, entity in pairs(ni.sense()) do
-                    if entity.key ==  "minecraft:fishing_bobber" then
-                        table.insert(seaLevels, entity.y)
+                for _, bobber in pairs(ni.sense()) do
+                    if bobber.key ==  "minecraft:fishing_bobber" then
+                        table.insert(seaLevels, bobber.y)
                     end
                 end
             end
         end
     end
     table.sort(seaLevels)
-    local level = seaLevels[math.ceil(dataCount / 2)]
-    if level then
-        local messageL = "Sealevel:"..level
-        c.clear()
-        c.addText({ cWidth - string.len(messageL) - x, y }, messageL, 0xFFFFFFFF, fontSize)
+    local average = math.ceil(dataCount / 2)
+    local level = seaLevels[average]
+    if math.abs(level - seaLevels[average + 1]) < 1 and
+    math.abs(level - seaLevels[average - 1]) < 1 then
+        log("Sealevel:"..level)
+        return level
     end
-    return level or nil
+    return nil
 end
-
 
 while true do
     meta = ni.getMetaOwner()
@@ -52,16 +56,14 @@ while true do
         if not seaLevel then
             seaLevel = findSeaLevel()
         else
+            local bobberFound = false
             for _, entity in pairs(ni.sense()) do
                 if entity.key ==  "minecraft:fishing_bobber" then
                     local diff = math.abs(entity.y - seaLevel)
 
-                    if logging then
-                        c.clear()
-                        c.addText({ cWidth - string.len(""..diff) - x, y }, ""..diff, 0xFFFFFFFF, fontSize)
-                    end
+                    if logging then log(tostring(diff)) end
 
-                    if diff > dipThreshhold then
+                    if diff > dipThreshold then
                         ni.use()
                         -- prevent fish entity from knocking bobber (other option would be to wait again)
                         ni.look(meta.yaw + (swap * turnRadius), meta.pitch)
@@ -70,21 +72,19 @@ while true do
                         -- prevent premature reeling in
                         os.sleep(castTime)
                         reeled = reeled + 1
-                        term.clear()
-                        term.setCursorPos(1,1)
-                        local messageR = "Reeled in: "..reeled
-                        print(messageR)
-                        c.clear()
-                        c.addText({ cWidth - string.len(messageR) - x, y }, messageR, 0xFFFFFFFF, fontSize)
+                        log("Reeled in: "..reeled)
                     end
+                    bobberFound = true
                 end
+            end
+
+            -- cast if server resets
+            if not bobberFound then
+                ni.use()
             end
         end
     else
         seaLevel = nil
-        if logging then
-            c.clear()
-            c.addText({ cWidth - string.len("Sea level nil") - x, y }, "Sea level nil", 0xFFFFFFFF, fontSize)
-        end
+        if logging then log("Sea level reset") end
     end
 end
